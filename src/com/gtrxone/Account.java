6ff3f416c6bba5d108ed0d0ba33b486c3994f75a
Goonawardana R N W <gtrxone@gmail.com>
2021-11-21 11:35:00 +0530

    Initial commit

---
+++ src/com/gtrxone/Account.java
@@ -0,0 +1,84 @@
+package com.gtrxone;
+
+public class Account {
+    private double balance;
+    private String number;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhoneNumber;
+
+    public Account() {
+        this("4521 5682 6556", 2500.00, "Default name", "Default email",
+                "Default phone");
+        System.out.println("Empty constructor called");
+    }
+
+    public Account(String number, double balance, String customerName, String customerEmail, String customerPhoneNumber) {
+        System.out.println("Account constructor with parameters called");
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public Account(String customerName, String customerEmail, String customerPhoneNumber) {
+        this("4565-5564-4452", 2500.00, customerName, customerEmail, customerPhoneNumber);
+    }
+
+    public void depositFunds(double amount) {
+        balance += amount;
+        System.out.println(amount + " deposited to yor account. New balance is " + balance);
+    }
+
+    public void withdrewFunds(double amount) {
+        if (balance > amount) {
+            balance -= amount;
+            System.out.println(amount + " withdrew form your account. Remaining account balance is " + balance);
+        } else {
+            System.out.println("Your account balance is low, You can cannot withdrew " + amount + "");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+
+}
---
+++ src/com/gtrxone/Main.java
@@ -0,0 +1,23 @@
+package com.gtrxone;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Account account = new Account();//"2265 5666 4568", 50000.00, "Gunawardna R N W",
+//                "gtrxone@gmail.com", "+94-704-888-610");
+//
+//        System.out.println(account.getBalance());
+//        account.depositFunds(7100);
+//        account.withdrewFunds(2500);
+//        account.withdrewFunds(60000);
+//        account.depositFunds(30000);
+//        account.withdrewFunds(60000);
+//
+//        Account rajithasAccount = new Account("Rajitha","rajitha.25@live.com",
+//                "0779851675");
+//        System.out.println(rajithasAccount.getNumber());
+//        System.out.println(rajithasAccount.getBalance());
+//        System.out.println(rajithasAccount.getNumber());
+
+    }
+}
